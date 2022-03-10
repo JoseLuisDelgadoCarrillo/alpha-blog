b88c73a51f853ebc5cb96d5bd6c963642f7b1477
Jose Luis Delgado Carrillo <joseluisdc48@gmail.com>
2022-03-10 14:21:15 -0700

    Se agrega tabla de listado

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 
 end
