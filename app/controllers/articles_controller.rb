e0523eca855f3d44c18edea8986a39ac1e6761a2
Jose Luis Delgado Carrillo <joseluisdc48@gmail.com>
2022-03-14 11:50:08 -0700

    Mesnajes, edit y update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,44 @@
 class ArticlesController < ApplicationController
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def index
         @articles = Article.all
     end
 
-    def show
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
         @article = Article.find(params[:id])
     end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+
+        if @article.save
+            flash[:notice] = "El articulo fue creado exitosamente"
+        redirect_to @article
+        else
+            render 'new'
+        end
+
+    end
+    
+    def update
+        @article  = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "El articulo fue actualizado correctamente."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+
 
 end
